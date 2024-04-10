d17ff404f6e507d3dcca03abd0a939c4f7d136ac
Richard han <hxy.314@gmail.com>
2024-04-10 22:52:59 +1000

    update test

--- app/src/features/HomePage.test.tsx
+++ app/src/features/HomePage.test.tsx
@@ -4,15 +4,53 @@ import { render, screen } from '@testing-library/react';
 import Home from './HomePage';
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
-const route = '/'
 
-test('renders state', () => {
-  render(
-    <MemoryRouter initialEntries={[route]}>
-      <Home />
-    </MemoryRouter>,
-  )
+const mockUseCategoryMock = jest.fn()
+jest.mock("../hooks/useCategory", () => ({
+  useCategory: () => mockUseCategoryMock(),
+}));
 
-  const linkElement = screen.getByText(/test/i);
-  expect(linkElement).toBeInTheDocument();
-});
+beforeAll(() => {
+  jest.mock("react-router-dom", () => ({
+    useParams: jest.fn(),
+  }));
+})
+
+describe("<Home />", () => {
+  it("displays loading state", () => {
+    mockUseCategoryMock.mockReturnValue({
+      data: { total: undefined, entries: undefined },
+      isFetching: true,
+      error: null,
+    });
+    const route = '/some-route'
+
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <Home />
+      </MemoryRouter>,
+    )
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("displays wrong state 2", () => {
+
+    jest.mock("../hooks/useCategory", () => ({
+      useCategory: () => mockUseCategoryMock(),
+    }));
+
+    mockUseCategoryMock.mockReturnValue({
+      data: { total: undefined, entries: undefined },
+      isFetching: false,
+      error: 'error',
+    });
+    const route = '/'
+
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <Home />
+      </MemoryRouter>,
+    )
+    expect(screen.getByText("Oops, something went wrong...")).toBeInTheDocument();
+  });
+})
--- app/src/features/HomePage.tsx
+++ app/src/features/HomePage.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Footer from '../components/footer/Index';
+import { useCategory } from '../hooks/useCategory';
+
 
 export const Home = () => {
+  const { isFetching, error } = useCategory();
+
+  const displayContent = () => {
+    if (isFetching) return <p>Loading...</p>;
+    if (error) return <p>Oops, something went wrong...</p>
+
+    return <>test</>
+  }
+
   return <>
     <Footer />
-    <div>Popular Titles</div>
+    {displayContent()}
   </>
 };
 
